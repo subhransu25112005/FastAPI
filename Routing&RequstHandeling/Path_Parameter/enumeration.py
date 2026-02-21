58ba2c81aabb66466fc6d5c838432f14c232b107
Subhranshu Nanda <subhransu25112005@gmail.com>
2026-02-21 15:05:34 +0530

    Add FastAPI endpoint for model names using Python enumerations

---
+++ Routing&RequstHandeling/Path_Parameter/enumeration.py
@@ -0,0 +1,104 @@
+'''Working with Python enumerations¶
+The value of the path parameter will be an enumeration member.
+
+Compare enumeration members¶
+You can compare it with the enumeration member in your created enum ModelName:
+
+
+Python 3.10+
+
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+Get the enumeration value¶
+You can get the actual value (a str in this case) using model_name.value, or in general, your_enum_member.value:
+
+
+
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+Tip
+
+You could also access the value "lenet" with ModelName.lenet.value.
+
+Return enumeration members¶
+You can return enum members from your path operation, even nested in a JSON body (e.g. a dict).
+
+They will be converted to their corresponding values (strings in this case) before returning them to the client:
+
+
+Python 3.10+
+
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+In your client you will get a JSON response like:
+
+
+{
+  "model_name": "alexnet",
+  "message": "Deep Learning FTW!"'''
